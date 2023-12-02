83b1c3edd07e85f43c32fdee448aef874319bdbf
wikanadnyana <wikanadnyana44@gmail.com>
2023-12-02 20:46:52 +0800

    add: news validation from request

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pickle
+from flask_cors import cross_origin
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+
+@app.route("/api/textscrape", methods=["POST"])
+@cross_origin()
+def textscrape():
+    #Validation for the link to make sure link is exist
+    data = request.get_json()
+    respon = requests.get(data["full_link"])
+    if respon.status_code != 200 :
+        return jsonify({"message": "News not found"}), 404
+    
+    return jsonify({"message": "News found and processed"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
